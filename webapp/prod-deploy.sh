091f827aefba89ac58ef387a2d097c9f1071eef9
Andrei Ghiuru <aghiuru@users.noreply.github.com>
2020-01-29 01:37:24 +0100

    Invalidate everything.

--- webapp/prod-deploy.sh
+++ webapp/prod-deploy.sh
@@ -2,4 +2,4 @@ sh prod-build.sh
 
 aws s3 sync --acl public-read --sse --delete resources/public/ s3://www.conjugate.app
 
-aws cloudfront create-invalidation --distribution-id E1FTNHB8ZZQTGK --paths "/js/compiled/app.js"
+aws cloudfront create-invalidation --distribution-id E1FTNHB8ZZQTGK --paths "/*"
